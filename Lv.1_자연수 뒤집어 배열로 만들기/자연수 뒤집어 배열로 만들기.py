822bd2b7f8c642d96465768817ca6f157b4c9539
DaanChoi <104367835+DaanChoi@users.noreply.github.com>
2022-06-02 15:26:53 +0900

    delete

--- Lv.1_자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++
@@ -1,7 +0,0 @@
-def solution(n):
-    return list(map(int, reversed(str(n))))
-print(solution(12345))
-
-# 아이디어
-# 뒤집기 => reversed()사용 : list뿐만 아니라 str 자료형도 사용 가능(reverse는 list만 사용 가능), 뒤집어진 객체를 반환하기 때문에 자료형을 설정해서 표현해야 함.
-# 리스트의 각 원소들 자료형 정수형으로 변환하기 => map() 사용
--- Lv.1_행렬의 덧셈/행렬의덧셈.py
+++
@@ -1,9 +0,0 @@
-def solution(arr1, arr2):
-    for i in range(len(arr1)):
-        for j in range(len(arr1)):
-            arr1[i][j] += arr2[i][j]
-    return arr1
-print(solution([[1,2],[2,3]], [[3,4],[5,6]]))
-
-#아이디어
-#행렬이 이차원 리스트이기 때문에 이중 for문으로 인덱싱
