5c990a9a08014744ec144377555494b262bfc008
DaanChoi <104367835+DaanChoi@users.noreply.github.com>
2022-06-05 15:15:29 +0900

    Update 문자열 내림차순.py

--- Level1/문자열 내림차순.py
+++ Level1/문자열 내림차순.py
@@ -2,7 +2,6 @@ def solution1(s):
     return ''.join(sorted(s, reverse=True))
 print(solution1("Zbcdefg"))
 
-def solution2(s):
-    s.sort()
-    return ''.join(s)
-print(solution2("Zbcdefg"))
+# 아이디어
+# 문자열 정렬 => sorted() 사용 : 문자열 정렬 후 리스트로 반환함 -> join으로 다시 문자열 만들어줌
+# sort()는 문자열은 안됨, 리스트만 됨
